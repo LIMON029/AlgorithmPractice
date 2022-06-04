9d262d7902021d0e80f25dc82c0720beccf21b0b
LIMON029 <limon00029@gmail.com>
2022-06-04 23:59:26 +0900

    boj 2750 EOF Error는 발생하나 출력은 정상

---
+++ boj_2750.py
@@ -0,0 +1,9 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+nums = []
+for i in range(n):
+    nums.append(int(input(sys.stdin.readline().rstrip())))
+nums.sort()
+for num in nums:
+    print(num)
